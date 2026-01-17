4246d003e70fa216a344ab092ecb60578daf36bc
Muragami <muragami@wishray.com>
2026-01-17 04:48:27 -0500

    Adjust formatting in example.c

--- test/example.c
+++ test/example.c
@@ -4,14 +4,17 @@
 int main(int argc, char *argv[])
 {
     ini_table_s* config = ini_table_create();
-    if (!ini_table_read_from_file(config, "test.ini")) {
+    if (!ini_table_read_from_file(config, "test.ini")) 
+    {
         puts("test.ini does not exist! Adding entries!");
         ini_table_create_entry(config, "Section", "one", "two");
         ini_table_create_entry(config, "Section", "; comment", "");
         ini_table_create_entry(config, "Section", "; comment 2", "");
         ini_table_create_entry(config, "SectionTwo", "key", "; val comment 2");
         ini_table_write_to_file(config, "test.ini");
-    }else {
+    }
+    else
+    {
         printf("Entry one is: %s\n", ini_table_get_entry(config, "Section", "one"));
         puts("creating copy to second.ini with comments");
         ini_table_create_entry(config, "Section", "; comment manually added", "");
